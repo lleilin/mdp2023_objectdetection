1a25b09ee9441446dc3444e28cce3213e674e41d
Lei Lin <leilin919228630@gmail.com>
2023-11-27 00:22:44 -0500

    Push FPS limit and ctrlc exit

--- video/main.py
+++ video/main.py
@@ -5,9 +5,10 @@ import threading
 import time
 import sys
 
+FPS = 5
 
 # Initialize the YOLO model
-model = YOLO("yolov8n-face.pt")
+model = YOLO("yolov8n.pt")
 
 # Initialize the audio recording settings
 chunk = 1024
@@ -82,18 +83,29 @@ def main():
     source = 0
     if len(sys.argv) > 1:
         source = sys.argv[1]
-    for result in model.track(
-        source=source, show=True, stream=True, agnostic_nms=True, save_crop=True, classes=0
-    ):
-        if len(result):
-            print("DETECTED")
-            frame_counter = 0  # Reset the frame counter when a face is detected
-            # Start recording audio in a separate thread
-            start_audio_recording()
-        else:
-            frame_counter += 1  # Increment the frame counter
-                
-    frame_counter = 11
+    try:
+        start_time = time.time()
+        for result in model.track(
+            source=source, show=True, stream=True, agnostic_nms=True, save_crop=True, classes=0
+        ):
+            if len(result):
+                print("DETECTED")
+                frame_counter = 0  # Reset the frame counter when a face is detected
+                # Start recording audio in a separate thread
+                start_audio_recording()
+            else:
+                frame_counter += 1  # Increment the frame counter
+
+            elapsed_time = time.time() - start_time
+            delay = max(0, 1 / FPS - elapsed_time)
+            time.sleep(delay)
+            start_time = time.time()
+
+    except KeyboardInterrupt:
+        print("\nCtrl+C detected. Exiting...")
+        frame_counter=11
+    finally:
+        terminate_audio_stream()
 
 
 # Terminate the audio stream
